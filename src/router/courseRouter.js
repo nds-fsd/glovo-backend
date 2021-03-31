ab5889054326bf198e9c2e21f3c8dd3d1365a90d
Patricio <patomunoz.mx@gmail.com>
2021-03-31 14:56:36 +0200

    First test complete Backend

---
+++ src/router/courseRouter.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const { CourseController } = require('../controller');
+
+const CourseRouter = express.Router();
+
+CourseRouter.get('/', CourseController.findAll);
+
+CourseRouter.get('/:id', CourseController.findOne);
+
+CourseRouter.post('/', CourseController.create);
+
+CourseRouter.put('/:id', CourseController.update);
+
+CourseRouter.delete('/:id', CourseController.deleteCourse);
+
+CourseRouter.delete('/deleteAll/:id', CourseController.deleteCoursesAndDishes)
+
+CourseRouter.post('/search', CourseController.search);
+
+module.exports = {CourseRouter}
---
+++ src/router/dishRouter.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const { DishController } = require('../controller');
+
+const DishRouter = express.Router();
+
+DishRouter.get('/', DishController.findAll);
+
+DishRouter.get('/:id', DishController.findOne);
+
+DishRouter.post('/', DishController.create);
+
+DishRouter.put('/:id', DishController.update);
+
+DishRouter.delete('/:id', DishController.deleteDish);
+
+
+DishRouter.post('/search', DishController.search);
+
+module.exports = {DishRouter}
