be973bf64f7d027cf75b7fa0f9fd847f06111e02
Patricio <patomunoz.mx@gmail.com>
2021-04-16 21:43:10 +0200

    Added JWT

--- src/mongo/schemas/user.js
+++ src/mongo/schemas/user.js
@@ -1,15 +1,28 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const secret = process.env.JWT_SECRET;
 
-const schema = new mongoose.Schema({
-	email: { type: String, required: true, unique: true},
+
+const userSchema = new mongoose.Schema({
+	email: { type: String, required: true, unique: true, trim: true},
 	password: {type: String, required: true},
-	name: {type: String, required: true}
-}, {timestamps: { createdAt: 'createdAt', updatedAt: 'updatedAt' }});
+	name: {type: String, required: true, trim: true}
+}, {timestamps: { createdAt: 'createdAt', updatedAt: 'updatedAt' }},
+{
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true } 
+  });
+userSchema.virtual('restaurant', {
+    ref: 'Restaurant',
+    localField: '_id',
+    foreignField: 'user', 
+    justOne: true,
+  });
 
 
 //esta funcion se ejecuta "antes" de guardar cualquier usuario en Mongo
-schema.pre('save',  function(next) {
+userSchema.pre('save',  function(next) {
 	const user = this;
 
 	//si no se ha cambiado la contraseña, seguimos
@@ -22,18 +35,36 @@ schema.pre('save',  function(next) {
 		bcrypt.hash(user.password, salt, function(err, hash) {
 			if (err) return next(err);
 
-			//si no ha habido error en el encryptado, guardamos
+			// si no ha habido error en el encryptado, guardamos
 			user.password = hash;
 			next();
 		});
 	});
 });
 
-schema.methods.comparePassword = function(password) {
+userSchema.methods.comparePassword = function(password) {
 	return bcrypt.compareSync(password, this.password);
 };
 
-const User = mongoose.model('User', schema);
+// * Method to generate the JWT (You choose the name)
+userSchema.methods.generateJWT = function() {
+	const today = new Date();
+	const expirationDate = new Date();
+
+	expirationDate.setDate(today.getDate() + 60);
+	
+	let payload = {
+		id: this._id,
+		name: this._name,
+		email: this._email,
+	};
+	// * This method is from the json-web-token library (who is in charge to generate the JWT
+	return jwt.sign(payload,secret, {
+		expiresIn: parseInt(expirationDate.getTime() / 1000, 10)
+	});
+};
+
+const User = mongoose.model('User', userSchema);
 
 
 
