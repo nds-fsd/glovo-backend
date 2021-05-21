54a504afa86acf779d14e3bc503d140bd4b68aa9
Patricio <patomunoz.mx@gmail.com>
2021-05-21 11:34:36 +0200

    added coordinates in restaurant schema

--- src/controller/restaurantController.js
+++ src/controller/restaurantController.js
@@ -42,7 +42,6 @@ exports.create = (req, res) => {
             return res.status(500).json({user: err.message})
         }
 
-        // * if the user has already a restaurant, he cant create more
        Restaurant.findOne({user: user._id},(err,restaurant) => {
            if (err) {
                return res.status(500).json({message: error});
@@ -62,8 +61,11 @@ exports.create = (req, res) => {
                        street: data.address.street,
                        zipcode: data.address.zipcode
                    },
+                   coordinates: data.coordinates,
+                   image: data.image,
                    restaurantCategory: data.restaurantCategory,
-                   user: user._id
+                   user: user._id,
+                   fullAddress: data.fullAddress
                })
                newRestaurant.save((err)=>{
                    if (err){ 
