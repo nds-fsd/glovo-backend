b58c225b1d18fe3ebc2911c19ab3b8f667635b17
DESKTOP-05JLC58\milec <milecofnas@gmail.com>
2021-06-03 15:43:16 +0200

    mailer

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 app.use(cors())
 const bodyParser = require("body-parser");
 const { configureSecurity, authRouter } = require('./security/jwtoken');
+const mailer = require('./mailer');
 
 const server = app.listen(process.env.PORT, () => {
 	console.log('server is running on port', server.address().port);
@@ -17,6 +18,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
 app.use("/",appRouter);
 app.use("/",authRouter);
+app.use("/send-email", mailer);
 
 
 
