cb528baff9d530adcab623fe0577edc0d33ec998
DESKTOP-05JLC58\milec <milecofnas@gmail.com>
2021-06-09 20:58:54 +0200

    mailer

--- src/mailer/index.js
+++ src/mailer/index.js
@@ -1,45 +1,56 @@
-// const nodemailer = require("nodemailer");
-// const ejs = require("ejs");
-// require("dotenv").config();
+const nodemailer = require("nodemailer");
+const ejs = require("ejs");
+const singUpHTML = require('../mailer/htmlTemplates/singUpTemplate');
+const orderCompleteHTML = require('../mailer/htmlTemplates/orderCompleteTemplate');
+require("dotenv").config();
+
+const transporter = nodemailer.createTransport({
+    port: 465,            
+    host: "smtp.gmail.com",
+    auth: {
+        user: process.env.THE_EMAIL,
+        pass: process.env.THE_PASSWORD,
+    },
+    secure: true,
+});
+
+function wrapedSendMail(mailOptions) {
+	return new Promise((resolve, reject) => {
+        mailOptions.from = process.env.THE_EMAIL;
+
+		transporter.sendMail(mailOptions, function (error, info) {
+			if (error) {
+				throw new Error(error);
+			} else {
+				resolve(info);
+			}
+		});
+	})
+}
+const sendSignUpEmail = (data, toEmail) => {
+	const html = ejs.render(singUpHTML, {data});
+	const mailData = {
+		to: toEmail,
+		html,
+		subject: "Welcome to Globo",
+	};
+
+	return wrapedSendMail(mailData);
+}
+const sendOrderCompleteEmail = (data, toEmail) => {
+	const html = ejs.render(orderCompleteHTML, {data});
+	const mailData = {
+		to: toEmail,
+		html,
+		subject: "We are preparing your order!",
+	};
+
+	return wrapedSendMail(mailData);
+}
 
 
-// async function wrapedSendMail(mailOptions) {
 
-// 	return new Promise((resolve, reject) => {
-
-// 		const transporter = nodemailer.createTransport({
-// 			port: 465,            
-// 			host: "smtp.gmail.com",
-// 			auth: {
-// 				user: process.env.THE_EMAIL,
-// 				pass: process.env.THE_PASSWORD,
-// 			},
-// 			secure: true,
-// 		});
-
-//         mailOptions.from = env.THE_EMAIL;
-
-// 		transporter.sendMail(mailOptions, function (error, info) {
-// 			if (error) {
-// 				throw new Error(error);
-// 			} else {
-// 				resolve(info);
-// 			}
-// 		});
-// 	})
-// }
-
-// const sendEmail = (data, htmlTemplate, from, to, subject ) => {
-// 	const html = ejs.render(htmlTemplate, {data});
-// 		const mailData = {
-// 			from,  // sender address
-// 			to,   // list of receivers
-// 			subject,
-// 			html
-// 		};
-// 		return wrapedSendMail(mailData);
-// };
-
-// module.exports = {
-// 	sendEmail
-// }
+module.exports = {
+	sendSignUpEmail,
+    sendOrderCompleteEmail
+}
