7617fb4aa23ac283230a5c7ee8605fc0510c05e6
FelixCat <felixyvmdor@gmail.com>
2021-06-09 13:39:45 +0200

    added phone number to user schema

--- src/mongo/schemas/user.js
+++ src/mongo/schemas/user.js
@@ -13,7 +13,8 @@ const userSchema = new mongoose.Schema({
 	email: { type: String, required: true, unique: true, trim: true},
 	password: {type: String, required: true},
 	firstName: {type: String, required: true},
-	lastName: {type: String},
+	lastName: { type: String },
+	phoneNumber: { type: String },
 	address: {
 		number: {type: String},
 		street:{type: String},
