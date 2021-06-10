7b813a97665570343a514fd271be435dfedf0d61
Patricio <patomunoz.mx@gmail.com>
2021-06-10 20:01:41 +0200

    added init super admin script

--- src/mongo/index.js
+++ src/mongo/index.js
@@ -6,6 +6,25 @@ const Course = require("./schemas/course.js");
 const Dish = require("./schemas/dish.js");
 const Order = require("./schemas/order.js");
 
+const initSuperAdmin = async () => {
+    const superAdmin = await User.find({email: 'super@admin.com'});
+    
+    if(superAdmin.length === 0) {
+        const admin = new User({
+            email: 'super@admin.com',
+            password: 'Admin123',
+            firstName: 'admin',
+            role: 'SUPER_ADMIN',
+          })
+          console.log('Creating SUPER_ADMIN')
+          return admin.save();
+    } else {
+        return null;
+    }
+}
+
+initSuperAdmin();
+
 module.exports = {
 	User,
 	RestaurantCategory,
